0115d7812ea5d1ff98cef7eeb2a849a25ca3ee0f
Emira Hajj <emirahajj@gmail.com>
2020-08-10 18:14:26 -0400

    Initial Commit

---
+++ tipster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipster
+//
+//  Created by Emira Hajj on 8/10/20.
+//  Copyright © 2020 Emira Hajj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
